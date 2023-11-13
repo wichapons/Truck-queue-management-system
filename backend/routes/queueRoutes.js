018c5af71bc737e62208e55666f827e9a8b2a78b
Wichapon V <wichapon@gmail.com>
2023-11-13 13:18:59 +0700

    Add update-docknumber and verification for restricted api

--- backend/routes/queueRoutes.js
+++ backend/routes/queueRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const verifyAuthToken = require('../middlewares/verifyAuthToken')
 const queueController = require("../controllers/queueController")
-/*
+
 // user routes
 router.use(verifyAuthToken.verifyIsLoggedIn) 
+/*
 router.get("/", orderController.getUserOrders)
 router.get("/user/:id", orderController.getOrderWithUserData);
 router.post("/", orderController.createOrder);
@@ -12,12 +13,14 @@ router.put("/paid/:id", orderController.updateOrderToPaid);
 */
 
 // admin routes
-//router.use(verifyAuthToken.verifyIsAdmin)
+//protect all routes below from non-admin access
+router.use(verifyAuthToken.verifyIsAdmin) 
+
 router.post("/send-message/:id", queueController.sendLineNotification);
-//router.put("/delivered/:id", orderController.updateOrderToDelivered);
 router.get("/", queueController.getAllQueue);
 router.post("/create", queueController.createNewQueue);
 router.put("/close/:id", queueController.closeQueue);
+router.put("/update-docknumber/:id", queueController.updateDockingNumber);
 //router.get("/analysis/:date", orderController.getOrderForAnalysis);
 
 module.exports = router
