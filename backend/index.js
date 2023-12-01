f8fd75e30181c9e66277c34de1e7c116ed19bdf9
Wichapon V <wichapon@gmail.com>
2023-12-01 11:33:43 +0700

    Increase payload size limit to 10MB

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,9 @@ const errorHandler = require("./middlewares/errorHandler")
 
 app.use(errorHandler)
 app.use(cookieParser());
-app.use(express.json());
+// Increase payload size limit to 10MB
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 
 app.use("/api", apiRoutes);
