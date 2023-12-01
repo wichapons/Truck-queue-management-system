926ec5d146572ed97fa5b814de240d0c6f5209f1
Wichapon V <wichapon@gmail.com>
2023-12-01 11:36:34 +0700

    Add funciton for converting .xlsx file to json format

--- backend/utils/uploadVendorData.js
+++ backend/utils/uploadVendorData.js
@@ -9,12 +9,36 @@ const workbook = XLSX.readFile(filePath);
 
 // Step 2: Convert the data to JSON format
 const sheet_name_list = workbook.SheetNames;
-let json_data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[1]]);
+let json_data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
 // Step 3: Iterate over the JSON array and create a new JSON file for all the records.
-fs.writeFileSync('all_vendors_SSP.json', JSON.stringify(json_data, null, 2), 'utf-8');
+//make key value to be lowercase
+json_data = json_data.map((item) => {
+  let newItem = {};
+  Object.keys(item).forEach((key) => {
+    newItem[key.toLowerCase()] = item[key];
+  });
+  return newItem;
+});
 
-console.log(json_data);
+fs.writeFileSync('all_vendors_CDS.json', JSON.stringify(json_data, null, 2), 'utf-8');
 
-//send json_data to mongodb
+/*
+// upload all_vendors_SSP.json to mongoDB
+
+// Step 6: Upload the data to MongoDB
+async function uploadDataToMongoDB() {
+  try {
+    await Supplier.insertMany(json_data);
+    console.log('Data uploaded to MongoDB successfully');
+  } catch (error) {
+    console.error('Error uploading data to MongoDB:', error);
+  } finally {
+    // Close the MongoDB connection
+    mongoose.connection.close();
+  }
+}
+
+uploadDataToMongoDB(json_data)
+*/
 
