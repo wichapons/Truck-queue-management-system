14bc42786d0a057b80090cf2169e397a0cfc104d
Wichapon V <wichapon@gmail.com>
2023-12-12 14:44:18 +0700

    Change endpoint to dev env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: '3000',
     proxy: {
       '/api': {
-        target: "https://truck-queue-management-uat-backend.onrender.com",
+        target: "http://localhost:8796/",
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, '')
       }
