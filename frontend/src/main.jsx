46f7cdb9224bbe9c9edf73f6f00b0c82ff19dfa9
Wichapon V <wichapon@gmail.com>
2023-12-13 16:10:42 +0700

    Fix typo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import './NotFoundPage.css'; 
+import './notFoundPage.css'; 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
