490f97344949708102c4698c220d39a95f118f58
Wichapon V <wichapon@gmail.com>
2023-12-24 22:36:28 +0700

    Add routes for RTV

--- backend/routes/queueRoutes.js
+++ backend/routes/queueRoutes.js
@@ -16,12 +16,24 @@ router.put("/paid/:id", orderController.updateOrderToPaid);
 //protect all routes below from non-admin access
 router.use(verifyAuthToken.verifyIsAdmin) 
 
-router.post("/send-message/:id", queueController.sendLineNotification);
+//GET REQUEST
+router.get("/rtv",queueController.getRTVQueue);
 router.get("/:productType", queueController.getAllQueue);
+
+
+//POST REQUEST
+router.post("/send-message/:id", queueController.sendLineNotification);
 router.post("/create", queueController.createNewQueue);
+
+//PUT REQUEST
 router.put("/close/:id", queueController.checkIn);
 router.put("/update-docknumber/:id", queueController.updateDockingNumber);
-router.put("/checkout/:id",queueController.checkOut)
+router.put("/checkout/:id",queueController.checkOut);
+
+router.put("/rtv/checkin/:id",queueController.checkInRTV);
+router.put("/rtv/checkout/:id",queueController.checkOutRTV);
+
 //router.get("/analysis/:date", orderController.getOrderForAnalysis);
 
-module.exports = router
+
+module.exports = router;
