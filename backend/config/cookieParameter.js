edc879cd9cb3d4b4dd9ad697e03dc0b7e08fd3e4
Wichapon V <wichapon@gmail.com>
2024-01-09 00:01:01 +0700

    Enable secure to "true" when on production to make sure it only sent over HTTPS and add domain in cookie config

--- backend/config/cookieParameter.js
+++ backend/config/cookieParameter.js
@@ -1,7 +1,8 @@
 const cookieParams = {
   httpOnly: true,
-  //secure: process.env.NODE_ENV === "production",
+  secure: process.env.NODE_ENV === "PROD",
   sameSite: "strict",
+  domain: process.env.NODE_ENV === "PROD" ? "truck-queue-uat.onrender.com":"localhost"
 };
 
 module.export= cookieParams;
