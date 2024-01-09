ab1ef908f851bb90ccb896b07b9ae90dca6ba020
Wichapon V <wichapon@gmail.com>
2024-01-09 10:24:16 +0700

    Add verifyAuthToken to supplier routes to protect routes from non-admin access

--- backend/routes/supplierRoutes.js
+++ backend/routes/supplierRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const supplierController = require("../controllers/supplierController")
+const verifyAuthToken = require("../middlewares/verifyAuthToken")
+
+router.use(verifyAuthToken.verifyIsLoggedIn) 
+//protect all routes below from non-admin access
+router.use(verifyAuthToken.verifyIsAdmin) 
 
 router.get("/:id", supplierController.getSupplierById);
 router.post("/newMultipleSup", supplierController.createMultipleSupplier);
